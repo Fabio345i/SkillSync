f84382d448ac02ed479e43590e6633739de9943f
Fabio345i <famor002@edu.cegepgarneau.ca>
2025-12-02 21:12:48 -0500

    Packages .net

--- backend/Api/Program.cs
+++ backend/Api/Program.cs
@@ -6,6 +6,11 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
